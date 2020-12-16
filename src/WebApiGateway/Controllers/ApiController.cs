c99767776c553a33590cf962c524557708f61bb2
植静 白 <842299086@qq.com>
2020-12-16 10:53:51 +0800

    添加项目文件。

---
+++ src/WebApiGateway/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiGateway.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/WebApiGateway/Controllers/GatewayApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiGateway.Controllers
+{
+    public class GatewayApiController :ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+        return "我是api网关！我是微服务架构中的唯一入口";
+        }
+    }
+}
